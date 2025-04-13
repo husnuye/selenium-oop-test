a6d259bf445bcb7f04bb900f6a470b91d4e82b03
Husnuye <husnuye_@hotmail.com>
2025-04-13 14:28:30 +0300

    Fix Chrome driver config for CI

--- Utils/DriverFactory.cs
+++ Utils/DriverFactory.cs
@@ -9,6 +9,13 @@ namespace SeleniumOOPTest.Utils
         {
             var options = new ChromeOptions();
             options.AddArgument("--start-maximized");
+            options.AddArgument("--headless"); // headless mod
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--remote-debugging-port=9222");
+            options.AddArgument($"--user-data-dir=/tmp/chrome-profile-{Guid.NewGuid()}");
+            
             return new ChromeDriver(options);
         }
     }
